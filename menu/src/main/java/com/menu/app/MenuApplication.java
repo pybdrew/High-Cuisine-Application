656be8427bec5aca46e04bfff22efd48f9c3cad7
pybdrew <116611820+pybdrew@users.noreply.github.com>
2025-03-27 00:25:45 +1000

    Progress on Login
    Worked on getting the layouts set up and creating a bit of the login form. Testing styles and behaviors of page.

--- menu/src/main/java/com/menu/app/MenuApplication.java
+++ menu/src/main/java/com/menu/app/MenuApplication.java
@@ -1,13 +1,15 @@
-package com.menu.menu;
+package com.menu.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.menu"})
 public class MenuApplication
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		SpringApplication.run(MenuApplication.class, args);
 	}
---
+++ menu/src/main/java/com/menu/controller/controller.java
@@ -0,0 +1,78 @@
+package com.menu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.menu.model.LoginModel;
+
+import jakarta.validation.Valid;
+
+
+@Controller
+public class controller
+{
+
+    /**
+     * Mapping for homepage
+     * @param model
+     * @return
+     */
+    @GetMapping("/home")
+    public String home(Model model)
+    {
+        model.addAttribute("title", "Home");
+        model.addAttribute("content", "Hello, welcome to the homepage!");
+        return "home";
+    }
+
+    /**
+     * Mapping for login
+     * @param model
+     * @return
+     */
+    @GetMapping("/login")
+    public String login(Model model)
+    {
+        model.addAttribute("loginModel", new LoginModel());
+        return "login";
+    }
+
+    /**
+     * Check fields for login
+     * @param loginModel
+     * @param bindingResult
+     * @param model
+     * @return
+     */
+    @PostMapping("/doLogin")
+    public String doLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model)
+    {
+        System.out.println(String.format("Form with username of %s and Password of %s", loginModel.getUsername(), loginModel.getPassword()));
+
+        if (bindingResult.hasErrors())
+        {
+            model.addAttribute("title", "Login Form");
+            return "login";
+        }
+
+        // Redirect to homepage after successful login
+        return "redirect:/home";
+    }
+
+    /**
+     * Mapping for register
+     * @param model
+     * @return
+     */
+    @GetMapping("/register")
+    public String register(Model model)
+    {
+        model.addAttribute("title", "Registration");
+        return "register";
+    }
+    
+}
+

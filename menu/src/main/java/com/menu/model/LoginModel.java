5bf4d0cc3d03db08ff93f3b163ebbd994398daf8
pybdrew <116611820+pybdrew@users.noreply.github.com>
2025-03-27 23:14:14 +1000

    Remodel Homepage/Create Registration
    Redesigned the homepage using Bootstrap to ensure it’s responsive. I also created the registration page using Bootstrap.

--- menu/src/main/java/com/menu/model/LoginModel.java
+++ menu/src/main/java/com/menu/model/LoginModel.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.*;
 public class LoginModel
 {
     @NotNull(message="User name is a required field")
-    @Size(min=1, max=32, message="User name must be between 1 and 32 characters")
+    @Size(min=1, max=32, message="User name must be between 8 and 20 characters")
     private String username;
 
     @NotNull(message="Password is a required field")
-    @Size(min=1, max=2, message="Password must be between 1 and 32 characters")
+    @Size(min=1, max=2, message="Password must be between 8 and 20 characters")
     private String password;
 
     public String getPassword()

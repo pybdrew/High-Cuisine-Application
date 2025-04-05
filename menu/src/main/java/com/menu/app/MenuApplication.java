1b5e6b8de0c26e236d5d5274f8b1704eaec72cb7
joenovotny <161538874+joenovotny@users.noreply.github.com>
2025-04-05 16:17:29 -0400

    Update MenuApplication.java

--- menu/src/main/java/com/menu/app/MenuApplication.java
+++ menu/src/main/java/com/menu/app/MenuApplication.java
@@ -1,5 +1,4 @@
 package com.menu.app;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;

33ae6e6b542fe4d0f3c346acc4b696dd6dbf6d39
pybdrew <116611820+pybdrew@users.noreply.github.com>
2025-05-11 12:03:56 -1000

    Update

--- menu/src/main/java/com/menu/controller/controller.java
+++ menu/src/main/java/com/menu/controller/controller.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.menu.data.entity.MenuEntity;
 import com.menu.data.entity.ProductEntity;
 import com.menu.data.repository.*;
